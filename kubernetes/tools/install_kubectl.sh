2f550f8c0883355dc50cc135ae4550266d480576
joshmeranda <joshmeranda@gmail.com>
2022-10-14 15:58:39 -0400

    add and satisfy tests for install_kubectl.sh

--- kubernetes/tools/install_kubectl.sh
+++ kubernetes/tools/install_kubectl.sh
@@ -11,7 +11,7 @@ usage="Usage: $(basename "$0") [-h] [-v <kubectl-version>] [-d <download-dir>] [
 args:
   -h                     display this help text
   -v <version>           the version of kubectl to install [$version]
-  -d <download-id>       the directory to download the kubectl binary to [$download_dir]
+  -d <download-dir>      the directory to download the kubectl binary to [$download_dir]
   -i <install-dir>       if different from the download location, a symlink to
                          the downloaded file will be created in this directory [$install_dir]
 "
@@ -24,7 +24,7 @@ while [ "$#" -gt 0 ]; do
       ;;
     -v)
       version="$2"
-      exit
+      shift
       ;;
     -d)
       download_dir="$2"
@@ -34,14 +34,19 @@ while [ "$#" -gt 0 ]; do
       install_dir="$2"
       shift
       ;;
+    *)
+      echo "unrecognized argument '$1'"
+      echo "$usage"
+      exit 1
+      ;;
   esac
   shift
 done
 
 download_url="https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl"
-checksum_url="https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl.sha256"
+checksum_url="https://dl.k8s.io/$version/bin/linux/amd64/kubectl.sha256"
 
-if [ -e "$download_dir" ]; then
+if [ -e "$download_dir/kubectl" ]; then
   log_info "using existing kubectl download at '$download_dir/kubectl'"
 else
   log_info "fetching kubectl from '$download_url'"
@@ -52,13 +57,16 @@ else
     exit 1
   fi
 
-  log_info "validating checksum form '$checksum_url'"
-  if ! curl --silent --location --output "$download_url/kubectl.sha256"; then
+  log_info "validating checksum from '$checksum_url'"
+  if ! curl --silent --location --output "$download_dir/kubectl.sha256" "$checksum_url"; then
     log_error 'could not fetch kubectl checksum'
     exit 1
   fi
 
-  if ! echo "$(cat kubectl.sha256) kubectl" | sha256sum --check; then
+  echo "$(cat "$download_dir/kubectl.sha256")" $download_dir/kubectl | sha256sum --status --check
+#  exit_code="$?"
+#  if [ "$?" -ne 0 ]; then
+  if ! echo "$(cat "$download_dir/kubectl.sha256")" $download_dir/kubectl | sha256sum --status --check; then
     log_error "checksum file '$download_dir/kubectl.sha256' does not match"
     exit 1
   fi
@@ -69,7 +77,7 @@ if [ ! -x "$download_dir/kubectl" ]; then
   chmod +x "$download_dir/kubectl"
 fi
 
-if [ "$download_dir" -ne "$install_dir" ]; then
+if [ "$download_dir" != "$install_dir" ]; then
   log_info "creating symlink to kubectl in '$install_dir'"
   if ! ln --symbolic "$download_dir/kubectl" "$install_dir"; then
     exit 1
