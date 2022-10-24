983d5012d034ce0eee71a42035400b6a417cf0cf
joshmeranda <joshmeranda@gmail.com>
2022-10-24 16:41:42 -0400

    only check for lines to ignore if requested

--- rulesets.rb
+++ rulesets.rb
@@ -8,30 +8,35 @@ rule 'MY000', 'A more strict version of MD013 to enforce line length limits excl
     # Increment :line_length to accommodate for unix line endings
     params[:line_length] += 1
 
-    # todo: we can easily optimize this by only searching if the element type is ignored
-    # Every line in the document that is part of a code block.
-    codeblock_lines = doc.find_type_elements(:codeblock).map do |e|
-      (doc.element_linenumber(e)..
-               doc.element_linenumber(e) + e.value.lines.count).to_a
-    end.flatten
-
-    # Every line in the document that is part of a table.
-    locations = doc.elements
-                   .map { |e| [e.options[:location], e] }
-                   .reject { |l, _| l.nil? }
-    table_lines = locations.map.with_index do |(l, e), i|
-      if e.type == :table
-        if i + 1 < locations.size
-          (l..locations[i + 1].first - 1).to_a
-        else
-          (l..doc.lines.count).to_a
-        end
-      end
-    end.flatten
-
     overlines = doc.matching_lines(/^.{#{@params[:line_length]}}.*$/)
-    overlines -= table_lines unless !params[:ignore_tables]
-    overlines -= codeblock_lines unless !params[:ignore_code_blocks]
+
+    # Every line in the document that is part of a code block.
+    if params[:ignore_code_blocks]
+      codeblock_lines = doc.find_type_elements(:codeblock).map do |e|
+        (doc.element_linenumber(e)..
+                 doc.element_linenumber(e) + e.value.lines.count).to_a
+      end.flatten
+
+      overlines -= codeblock_lines
+    end
+
+    if params[:ignore_tables]
+      # Every line in the document that is part of a table.
+      locations = doc.elements
+                     .map { |e| [e.options[:location], e] }
+                     .reject { |l, _| l.nil? }
+      table_lines = locations.map.with_index do |(l, e), i|
+        if e.type == :table
+          if i + 1 < locations.size
+            (l..locations[i + 1].first - 1).to_a
+          else
+            (l..doc.lines.count).to_a
+          end
+        end
+      end.flatten
+
+      overlines -= table_lines
+    end
 
     # We want to ignore files that exceed the line length limit, but end in a link.
     #  - [link text](some-url "Some Title")
